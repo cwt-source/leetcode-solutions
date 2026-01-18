9fd0c3840526e65d7a6ebc0d3949af3b1b3662de
18276533678 <15127820+chenweiting123@user.noreply.gitee.com>
2026-01-19 01:36:39 +0800

    添加第一道题：两数之和(Python)

---
+++ Python/0001-two-sum.py
@@ -0,0 +1,10 @@
+# 0001-two-sum.py
+# 两数之和 (Two Sum)
+class Solution:
+    def twoSum(self, nums, target):
+        seen = {}
+        for i, num in enumerate(nums):
+            if target - num in seen:
+                return [seen[target - num], i]
+            seen[num] = i
+        return []
